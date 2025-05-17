de485f99febd02b6c64038c67ab60568ee8462dd
ElegAlex <ab@alexandre-berge.fr>
2025-05-17 11:42:19 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+            import React from 'react';
+            import ReactDOM from 'react-dom/client';
+            import './index.css'; // Assurez-vous que ce chemin est correct
+            import App from './App'; // Assurez-vous que ce chemin est correct
+
+            const root = ReactDOM.createRoot(document.getElementById('root'));
+            root.render(
+              <React.StrictMode>
+                <App />
+              </React.StrictMode>
+            );
+            
